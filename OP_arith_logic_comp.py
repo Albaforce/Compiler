e8b18ab6489803c09d3b833a96986c75b2559ef5
albaforce <raoufusma500@gmail.com>
2024-11-12 17:00:37 +0100

    LEXICAL ANALYSIS

---
+++ OP_arith_logic_comp.py
@@ -0,0 +1,81 @@
+import re 
+
+ADD_OPERATOR = r'\+'
+SUB_OPERATOR = r'-'
+MULTIPLY_OPERATOR = r'\*'
+DIV_OPERATOR = r'/'
+
+ARITHMETIC_PATTERN = f"{ADD_OPERATOR}|{SUB_OPERATOR}|{MULTIPLY_OPERATOR}|{DIV_OPERATOR}"
+
+AND_OPERATOR = r'&&'        
+OR_OPERATOR = r'\|\|'       
+NOT_OPERATOR = r'!'         
+
+LOGIC_PATTERN = f"{AND_OPERATOR}|{OR_OPERATOR}|{NOT_OPERATOR}"
+
+GREATER_THAN = r'>'
+LESS_THAN = r'<'
+GREATER_EQUAL = r'>='
+LESS_EQUAL = r'<='
+EQUALS = r'=='
+NOT_EQUALS = r'!='
+
+CONDITION_PATTERN = f"{GREATER_THAN}|{LESS_THAN}|{GREATER_EQUAL}|{LESS_EQUAL}|{EQUALS}|{NOT_EQUALS}"
+
+def lexer(input_string):
+    tokens = []  
+    matches = re.finditer(ARITHMETIC_PATTERN, input_string)
+    for match in matches:
+        if match.group() == '+':
+            token = {'type': 'ADD_OPERATOR', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '-':
+            token = {'type': 'SUB_OPERATOR', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '*':
+            token = {'type': 'MULTIPLY_OPERATOR', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '/':
+            token = {'type': 'DIV_OPERATOR', 'value': match.group()}
+            tokens.append(token)
+
+    matches = re.finditer(LOGIC_PATTERN, input_string)
+    for match in matches:
+        if match.group() == '&&':
+            token = {'type': 'AND_OPERATOR', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '||':
+            token = {'type': 'OR_OPERATOR', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '!':
+            token = {'type': 'NOT_OPERATOR', 'value': match.group()}
+            tokens.append(token)
+
+    matches = re.finditer(CONDITION_PATTERN, input_string)
+    for match in matches:
+        if match.group() == '>':
+            token = {'type': 'GREATER_THAN', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '<':
+            token = {'type': 'LESS_THAN', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '>=':
+            token = {'type': 'GREATER_EQUAL', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '<=':
+            token = {'type': 'LESS_EQUAL', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '==':
+            token = {'type': 'EQUALS', 'value': match.group()}
+            tokens.append(token)
+        elif match.group() == '!=':
+            token = {'type': 'NOT_EQUALS', 'value': match.group()}
+            tokens.append(token)
+    
+    return tokens
+
+input_string = " 3 + 5 * 2 + w && x || y > z == 10"
+tokens = lexer(input_string)
+
+for token in tokens:
+    print(token)
